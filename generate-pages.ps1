3f4d1fd9517ab71a29abe755c2ff7925f8fada97
Augusto César Bisognin <augustocb@gmail.com>
2021-02-03 18:55:44 -0300

    generate documents on function

--- generate-pages.ps1
+++ generate-pages.ps1
@@ -16,40 +16,41 @@ function CopyStaticFiles {
     }
 
     $outFolder = New-Item $htmlOut -ItemType Directory
-    $files = (Get-Item $htmlStatic).GetFiles()
-    foreach ($file in $files) {
-        $percent = $files.IndexOf($file) / $files.Length * 100
-        Write-Progress -Id 1 -Activity 'Copying static content...' -Status $file.Name -PercentComplete $percent
-
+    foreach ($file in (Get-Item $htmlStatic).GetFiles()) {
         Copy-Item $file.FullName $outFolder
     }
 }
 
+function New-XmlPage([string] $Id, [string] $Title) {
+    $doc = New-Object System.Xml.XmlDocument
+    $doc.InsertBefore($doc.CreateXmlDeclaration('1.0', 'UTF-8', $null), $doc.DocumentElement) | Out-Null
+    $doc.AppendChild($doc.CreateProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="page.xsl"')) | Out-Null
+
+    $pageNode = $doc.CreateElement('page')
+    $pageNode.SetAttribute('id', $Id)
+    $pageNode.SetAttribute('title', $Title)
+
+    $doc.AppendChild($pageNode) | Out-Null
+    return $doc
+}
+
 function GenerateEmigrantsList {
     $emigrants = Select-Xml -Path $dataFile -XPath 'Museu/IdentificacaoEmigrante'
-
-    $newXml = New-Object System.Xml.XmlDocument    
-    $newXml.InsertBefore($newXml.CreateXmlDeclaration('1.0', 'UTF-8', $null), $newXml.DocumentElement)
-    $newXml.AppendChild($newXml.CreateProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="page.xsl"'))
-
-    $pageNode = $newXml.CreateElement('page')
-    $pageNode.SetAttribute('id', 'emigrants')
-    $pageNode.SetAttribute('title', 'Emigrantes')
+    $doc = New-XmlPage -Id 'emigrants' -Title 'Emigrantes'
 
     foreach ($emigrant in $emigrants) {
-        $node = $newXml.CreateElement('plaintext')
+        $node = $doc.CreateElement('plaintext')
         $node.InnerText = Select-Xml -Xml $emigrant.Node -XPath 'nome/text()'
 
-        $pageNode.AppendChild($node)
+        $doc.DocumentElement.AppendChild($node) | Out-Null
     }
 
-    $newXml.AppendChild($pageNode)
-    $newXml.Save("$htmlOut/emigrants.xml")
+    $doc.Save("$htmlOut/emigrants.xml")
 }
 
 CopyStaticFiles
 GenerateEmigrantsList
 
 # TODO remover pausa
-Pause
+#Pause
 Start-Process "$htmlOut/index.html"
